a673e5a9acd2c7778bb8b7f35d8752a55575d857
Jean-Philippe Lapierre <jean-philippe@roostermoney.com>
2021-09-27 15:54:35 -0400

    Refactors App to become functional component; Consolidates cart item quantities

--- src/App.js
+++ src/App.js
@@ -1,132 +1,154 @@
-import React from 'react';
-
-import './App.css';
+import { useReducer, useEffect } from "react";
+import "./App.css";
 
 let products = [
   {
     id: 1,
     title: "Pocket Knife",
     price: 35,
-    imageUrl: "https://i0.wp.com/scoutingmagazine.org/wp-content/uploads/2013/02/SwissArmyKnife.jpg?w=550&ssl=1"
+    imageUrl:
+      "https://i0.wp.com/scoutingmagazine.org/wp-content/uploads/2013/02/SwissArmyKnife.jpg?w=550&ssl=1",
   },
   {
     id: 2,
     title: "Trail Mix",
     price: 12,
-    imageUrl: "https://m.media-amazon.com/images/I/91P47phWyUL._AC_SL1500_.jpg"
+    imageUrl: "https://m.media-amazon.com/images/I/91P47phWyUL._AC_SL1500_.jpg",
   },
   {
     id: 3,
     title: "Rain Gear",
     price: 59,
-    imageUrl: "https://i2.wp.com/scoutingmagazine.org/wp-content/uploads/2013/02/MarmotPrecip.jpg?w=550&ssl=1"
+    imageUrl:
+      "https://i2.wp.com/scoutingmagazine.org/wp-content/uploads/2013/02/MarmotPrecip.jpg?w=550&ssl=1",
   },
-]
+];
 
-function LoadProducts() {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(products), 500)
+function loadProducts() {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(products), 500);
   });
 }
 
-class App extends React.Component {
-  state = {
+function stateReducer(state, action) {
+  const { type } = action;
+  switch (type) {
+    case "START_FETCHING_PRODUCTS":
+      return { ...state, loading: action.loading };
+    case "RECEIVE_PRODUCTS":
+      return { ...state, loading: false, products: action.products };
+    case "ADD_ITEM_TO_CART":
+      const updatedItems = state.items
+        .concat( action.item)
+        .reduce((items, product) => {
+        if (items[product.title]) {
+          items[product.title].qty += 1;
+        } else {
+          items[product.title] = { ...product, qty: 1 };
+        }
+        return items;
+      }, {});
+      return { ...state, items: Object.values(updatedItems) };
+    case "CLEAR_CART":
+        return { ...state, items: [] };
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [{ loading, products, items }, dispatch] = useReducer(stateReducer, {
     items: [],
     products: undefined,
-    l: true,
-  }
+    loading: true,
+  });
 
-  componentDidMount() {
-    this.setState({l: true});
+  useEffect(() => {
+    dispatch({ type: "START_FETCHING_PRODUCTS", loading: true });
 
-    let that = this;
-    LoadProducts().then(function (products) {
-      that.setState({l: false, products});
+    loadProducts().then((products) => {
+      dispatch({ products, type: "RECEIVE_PRODUCTS" });
     });
+  }, []);
+
+  console.log("state", { loading, products, items });
+
+  if (loading) {
+    return <div>'Loading...'</div>;
   }
-
-  render() {
-    console.log('this.state', this.state);
-
-    if (!this.state.l) {
-      return (
-        <div className="main">
-          <header className="main-header">
-            <h1>Scout Essentials</h1>
-          </header>
-          <div>
-            <h1>Product List</h1>
-            <div className="product-list">
-              {
-                this.state.products.map((x) => {
-                  return (
-                    <div className="product">
-                      <p>{x.title}</p>
-                      <p>${x.price}</p>
-                      <div>
-                        <img className="product-image" src={x.imageUrl} alt={x.title}/>
-                      </div>
-                      <button
-                        className="btn"
-                        onClick={() => {
-                          console.log('add', x);
-                          let pr = {
-                            title: x.title,
-                            price: x.price
-                          }
-                          this.setState({items: [...this.state.items, pr]})
-                        }}
-                      >
-                        Add to Cart
-                      </button>
-                    </div>
-                  )
-                })
-              }
-            </div>
-          </div>
-          <div className="cart">
-            <h3>Your Cart</h3>
-            <p>Items in cart: {this.state.items.length}</p>
-            {this.state.items.map(x => {
-              return (
+  return (
+    <div className="main">
+      <header className="main-header">
+        <h1>Scout Essentials</h1>
+      </header>
+      <div>
+        <h1>Product List</h1>
+        <div className="product-list">
+          {products.map((x) => {
+            return (
+              <div className="product">
+                <p>{x.title}</p>
+                <p>${x.price}</p>
                 <div>
-                  {x.title}
+                  <img
+                    className="product-image"
+                    src={x.imageUrl}
+                    alt={x.title}
+                  />
                 </div>
-              )
-            })}
-            <p>Total price: ${this.state.items.reduce((prevValue, currValue) => {
-              return prevValue + currValue.price
-            }, 0)}</p>
-            <div className="buttons">
-              <button
-                className="btn"
-                onClick={() => {
-                  this.setState({items: []})
-                }}>
-                Clear
-              </button>
-              <button
-                className="btn"
-                onClick={() => alert('Not there yet')}
-              >
-                Checkout
-              </button>
-            </div>
-          </div>
-          <footer>
-            <hr/>
-            <p>
-              <span>Scout Essentials by CSC Engineering, 2021. </span>
-              <a href="https://jobs-cscgeneration.icims.com/jobs">Join Us!</a>
-            </p>
-          </footer>
+                <button
+                  className="btn"
+                  onClick={() => {
+                    console.log("add", x);
+                    const item = {
+                      title: x.title,
+                      price: x.price,
+                    };
+                    dispatch({ type: "ADD_ITEM_TO_CART", item });
+                  }}
+                >
+                  Add to Cart
+                </button>
+              </div>
+            );
+          })}
         </div>
-      );
-    }
-
-    return 'Loading...'
-  }
+      </div>
+      <div className="cart">
+        <h3>Your Cart</h3>
+        <p>Items in cart: {items.length}</p>
+        {items.map(({ title }) => {
+          return <div>{title}</div>;
+        })}
+        <p>
+          Total price: $
+          {items.reduce((prevValue, currValue) => {
+            return prevValue + currValue.price;
+          }, 0)}
+        </p>
+        <div className="buttons">
+          <button
+            className="btn"
+            onClick={() => {
+              dispatch({ type: "CLEAR_CART" });
+            }}
+          >
+            Clear
+          </button>
+          <button className="btn" onClick={() => alert("Not there yet")}>
+            Checkout
+          </button>
+        </div>
+      </div>
+      <footer>
+        <hr />
+        <p>
+          <span>Scout Essentials by CSC Engineering, 2021. </span>
+          <a href="https://jobs-cscgeneration.icims.com/jobs">Join Us!</a>
+        </p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
